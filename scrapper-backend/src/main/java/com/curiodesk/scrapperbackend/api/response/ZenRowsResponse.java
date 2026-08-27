ae4d8f892880724d6dee2af66f2a0eafc3b23e7c
anand-aman <itsamananand@gmail.com>
2026-08-27 14:22:21 +0530

    json & pojos

---
+++ scrapper-backend/src/main/java/com/curiodesk/scrapperbackend/api/response/ZenRowsResponse.java
@@ -0,0 +1,78 @@
+package com.curiodesk.scrapperbackend.api.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record ZenRowsResponse(
+        String html,
+        Parsed parsed
+) {
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record Parsed(
+            @JsonProperty("current_position")
+            String currentPosition,
+
+            List<Education> education,
+
+            List<Experience> experience,
+
+            List<String> languages,
+
+            Member member,
+
+            List<Post> posts
+    ) {
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record Member(
+            @JsonProperty("connection_count_label")
+            String connectionCountLabel,
+
+            @JsonProperty("country_code")
+            String countryCode,
+
+            @JsonProperty("cover_image_url")
+            String coverImageUrl,
+
+            @JsonProperty("follower_count")
+            Integer followerCount,
+
+            @JsonProperty("full_name")
+            String fullName,
+
+            String location,
+
+            @JsonProperty("profile_photo_url")
+            String profilePhotoUrl,
+
+            @JsonProperty("vanity_url")
+            String vanityUrl
+    ) {
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record Post(
+            @JsonProperty("date_published")
+            String datePublished,
+
+            @JsonProperty("like_count")
+            Integer likeCount,
+
+            @JsonProperty("post_url")
+            String postUrl,
+
+            String text
+    ) {
+    }
+
+    public record Education() {
+    }
+
+    public record Experience() {
+    }
+}
