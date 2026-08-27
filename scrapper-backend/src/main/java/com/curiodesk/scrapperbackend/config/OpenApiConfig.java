6e164b805d3f0b549a8e226bb74a20149b606962
anand-aman <itsamananand@gmail.com>
2026-08-27 19:29:05 +0530

    swagger documentation

---
+++ scrapper-backend/src/main/java/com/curiodesk/scrapperbackend/config/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package com.curiodesk.scrapperbackend.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Scrapper Backend API",
+                version = "v1",
+                description = "API for scraping LinkedIn profile data",
+                contact = @Contact(name = "Scrapper Backend")
+        ),
+        servers = @Server(url = "/", description = "Default server")
+)
+public class OpenApiConfig {
+}
